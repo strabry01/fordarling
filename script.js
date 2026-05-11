6e85c2626d8b8993ce4a6032812493a8b186d6aa
saint <vrizelea@gmail.com>
2026-05-12 00:37:15 +0800

    la pa cake tu

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const okayBtn = document.getElementById("okayBtn");
 
+const firstScreen = document.getElementById("firstScreen");
+
+const secondScreen = document.getElementById("secondScreen");
+
+
+okayBtn.addEventListener("click", function(){
+
+    firstScreen.style.display = "none";
+
+    secondScreen.style.display = "flex";
+
+});
