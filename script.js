6219a743c2c8b316841bd42fd8e1d9a6d121b040
saint <vrizelea@gmail.com>
2026-06-05 20:14:39 +0800

    i miss my wife

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const okayBtn = document.getElementById("okayBtn");
-
 const firstScreen = document.getElementById("firstScreen");
-
 const secondScreen = document.getElementById("secondScreen");
 
-
 okayBtn.addEventListener("click", function(){
-
     firstScreen.style.display = "none";
-
     secondScreen.style.display = "flex";
-
+    secondScreen.classList.add("active");
 });
